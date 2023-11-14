2254d9d267cf606c3fd3fd916c269c29d1ef842c
Tuan Tran (Hugo) <tuan.tran.trong@outlook.com>
2023-11-14 07:43:51 -0500

    Modify model & open AI connect

--- app/api/v1/text_generation/text_generation_model.py
+++ app/api/v1/text_generation/text_generation_model.py
@@ -1,9 +1,34 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 # Define your Pydantic models (schemas) here
 
 class TextGenerationInput(BaseModel):
-    input_text: str
+    input_text: str = Field(max_length=300, 
+                            title="TextGenerationInput",
+                            description="Input text from user, cannot exceed 300 characters")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "input_text": "Salon U is an award-winning salon, established in the year 2002 and known for high-quality beauty, hair, and spa services."
+                }
+            ]
+        }
+    }
 
 class TextGenerationOutput(BaseModel):
-    generated_text: str
+    generated_texts: list[str] = Field(description="Generated text from the service. Provide multiple option for user")
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "generated_texts": [
+                        "Salon U - Elevate Your Beauty Experience with Our Award-Winning Hair, Beauty and Spa Services Since 2002",
+                        "Explore Salon U - Where Excellence Meets Beauty. Offering Premium Hair, Beauty, and Spa Services for Nearly 20 Years"
+                    ]
+                }
+            ]
+        }
+    }
--- app/config/connect_openai.py
+++ app/config/connect_openai.py
@@ -2,14 +2,13 @@ from openai import OpenAI
 from os import getenv
 
 OPENAI_CLIENT = None
-ORGANIZATION_ID = "org-MmAlb4d7l2IzkGO3xcVrYZLV" # Sliike Test from OpenAI's account
 
 def connect_OpenAI():
     global OPENAI_CLIENT
     if OPENAI_CLIENT is None:
         # Provide OpenAI's API KEY to connect to OpenAI's API using openai package
         OPENAI_API_KEY = getenv('OPENAI_API_KEY')
-
+        ORGANIZATION_ID = getenv('OPENAI_ORGANIZATION_ID') # Organization ID from OpenAI's account, in Settings 
         # Create connection to OpenAI's API
         OPENAI_CLIENT = OpenAI(api_key=OPENAI_API_KEY,
                                organization=ORGANIZATION_ID)
